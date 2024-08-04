46775273151df9faef9e5b19891c7c34cf70760f
Iman <imangholamiimi@gmail.com>
2024-08-04 18:49:19 +0330

    :tada: | START | starting project | SETUP SCREEN

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+
+# Screen Setups
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title("Pong Game")
+screen.exitonclick()
