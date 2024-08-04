788613230e2a07f0c3f17ac21920904757ae71b3
Iman <imangholamiimi@gmail.com>
2024-08-04 20:10:52 +0330

    :zap: | UPDATE | change font style and score counters

--- score_board.py
+++ score_board.py
@@ -1,7 +1,8 @@
 from turtle import Turtle
 
 ALIGNMENT = "center"
-FONT = ("Courier", 20, "normal")
+FONT = ("Arial", 20, "normal")
+
 
 class ScoreBoard(Turtle):
     def __init__(self):
@@ -10,12 +11,15 @@ class ScoreBoard(Turtle):
         self.color("white")
         self.hideturtle()
         self.penup()
-        self.setpos(x=0, y=300)
-        self.write(arg=f"SCORE: {self.score}", align=ALIGNMENT, font=FONT)
+        self.setpos(x=-30, y=300)
+        self.write(arg=f"{self.score}", align=ALIGNMENT, font=FONT)
+        self.setpos(x=30, y=300)
+        self.write(arg=f"{self.score}", align=ALIGNMENT, font=FONT)
         self.draw_vertical_line()
         self.draw_horizontal_line()
 
     def draw_vertical_line(self):
+        self.teleport(x=0, y=300)
         self.setheading(-90)
         while self.ycor() > -400:
             self.pensize(width=3)
