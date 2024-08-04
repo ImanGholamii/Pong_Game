44d0c5a95820aca9938fc3000577700d87239df4
Iman <imangholamiimi@gmail.com>
2024-08-04 20:45:33 +0330

    :zap: | UPDATE | Added Event Listeners to move up/down pddles | change tracer() to show better animation

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from turtle import Turtle, Screen
 from score_board import ScoreBoard
-# objects
-screen = Screen()
-score_board = ScoreBoard()
+from paddles import Paddle
 
 # Screen Setups
+screen = Screen()
 screen.setup(width=1200, height=660)
 screen.bgcolor('black')
 screen.title("Pong Game")
 screen.tracer(0, 0)
 
+# objects
+score_board = ScoreBoard()
+right_paddle = Paddle()
+screen.tracer(1, 10)
+right_paddle.right_paddle()
+left_paddle = Paddle()
+left_paddle.left_paddle()
+# key events
+screen.tracer(2, 0)
+screen.listen()
+screen.onkey(right_paddle.up, 'Up')
+screen.onkey(right_paddle.down, 'Down')
+screen.onkey(left_paddle.up, 'W')
+screen.onkey(left_paddle.down, 'S')
+
+
+screen.update()
 screen.exitonclick()
