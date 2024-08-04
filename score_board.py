7dab88f032a980ef5d249094ceea7315ea034614
Iman <imangholamiimi@gmail.com>
2024-08-04 19:51:01 +0330

    :zap: | UPDATE | added horizontal board line

--- score_board.py
+++ score_board.py
@@ -12,9 +12,10 @@ class ScoreBoard(Turtle):
         self.penup()
         self.setpos(x=0, y=300)
         self.write(arg=f"SCORE: {self.score}", align=ALIGNMENT, font=FONT)
-        self.draw_line()
+        self.draw_vertical_line()
+        self.draw_horizontal_line()
 
-    def draw_line(self):
+    def draw_vertical_line(self):
         self.setheading(-90)
         while self.ycor() > -400:
             self.pensize(width=3)
@@ -23,3 +24,10 @@ class ScoreBoard(Turtle):
             self.penup()
             self.fd(20)
 
+    def draw_horizontal_line(self):
+        self.teleport(x=-600, y=300)
+        while self.xcor() < 600:
+            self.setheading(0)
+            self.pensize(width=3)
+            self.pendown()
+            self.fd(20)
