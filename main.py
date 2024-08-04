4459706516c209b710ad8201fd1d789f7ea066d7
Iman <imangholamiimi@gmail.com>
2024-08-04 19:40:37 +0330

    :zap: | UPDATE screen size | :sparkles: | ADD score board

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from turtle import Turtle, Screen
-
+from score_board import ScoreBoard
+# objects
 screen = Screen()
+score_board = ScoreBoard()
 
 # Screen Setups
-screen.setup(width=800, height=600)
+screen.setup(width=800, height=650)
 screen.bgcolor('black')
 screen.title("Pong Game")
+screen.tracer(0, 0)
+
 screen.exitonclick()
