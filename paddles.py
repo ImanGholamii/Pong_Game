8c7db856ab57a79ab14fb946ac64c306ab1b913f
Iman <imangholamiimi@gmail.com>
2024-08-05 18:25:55 +0330

    :zap: | UPDATE | added position argument to initializer and removed left/right_paddle methods

--- paddles.py
+++ paddles.py
@@ -1,18 +1,14 @@
 from turtle import Turtle
 
+
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.color("white")
         self.shape('square')
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-
-    def right_paddle(self):
-        self.goto(x=550, y=0)
-
-    def left_paddle(self):
-        self.goto(x=-550, y=0)
+        self.goto(position)
 
     def up(self):
         if self.ycor() < 220:
