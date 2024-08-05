210a0abf7d4f1fbcd7e756253e4cfe49160bf9d1
Iman <imangholamiimi@gmail.com>
2024-08-05 12:32:51 +0330

    :ambulance: | FIX | 'w' and 's' keys

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ screen.tracer(2, 0)
 screen.listen()
 screen.onkey(right_paddle.up, 'Up')
 screen.onkey(right_paddle.down, 'Down')
-screen.onkey(left_paddle.up, 'W')
-screen.onkey(left_paddle.down, 'S')
+screen.onkey(left_paddle.up, 'w')
+screen.onkey(left_paddle.down, 's')
 
 
 screen.update()
