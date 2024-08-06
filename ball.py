b4d0eb26eab288f35cb8e61e721d97436ec0cc09
Iman <imangholamiimi@gmail.com>
2024-08-06 11:27:17 +0330

    :hammer: | Rewrite | change the logic of move and added bounce method

--- ball.py
+++ ball.py
@@ -1,41 +1,23 @@
 from turtle import Turtle
 from random import randint
 
+
 class Ball(Turtle):
+    random_number = randint(0, 360)
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.color("white")
-        # self.speed("fast")
         self.penup()
-        self.start()
-        self.move()
-
-    def start(self):
-        if self.position() == (0, 0):
-            self.setheading(randint(0, 180))
-            self.fd(40)
-            while self.position() != (0, 0):
-                self.fd(40)
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        """detect boards and Collision up and down corners"""
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
-        if self.xcor() < 590 and self.xcor() > -590 and self.ycor() < 390 and self.ycor() > -390:
-            if self.heading() < 90 or self.heading() > 0:
-                self.setheading(randint(-89, 1))
-                self.fd(40)
-            elif self.heading() < 0 or self.heading() > -90:
-                self.setheading(randint(0, 89))
-                self.fd(40)
-            elif self.heading() < 180 or self.heading() > 90:
-                self.setheading(randint(89, 179))
-                self.fd(40)
-            elif self.heading() < 270 or self.heading() > 180:
-                self.setheading(randint(89, 179))
-            else:
-                self.fd(40)
-        elif self.ycor() >= 390 or self.ycor() <= -390:
-            pass
-
-
+    def bounce(self):
+        """Reverse the x/y coordinates"""
+        self.x_move *= -1
+        self.y_move *= -1
