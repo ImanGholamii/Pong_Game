df2e314e51aabc8caf9e1851d47c292f9faaacc7
Iman <imangholamiimi@gmail.com>
2024-08-07 18:27:18 +0330

    :zap: | UPDATE | added increase ball speed to collision with paddles

--- main.py
+++ main.py
@@ -47,15 +47,14 @@ while game_is_on:
 
     if ball.xcor() > 530 and ball.distance(right_paddle) < 50 or ball.xcor() < -530 and ball.distance(left_paddle) < 50:
         ball.bounce_x()
+        ball.increase_speed()
     elif ball.xcor() > 550:
         ball.restart()
-        ball.increase_speed()
         ball.bounce_x()
         left_score_board.clear()
         left_score_board.add_point()
     elif ball.xcor() < -550:
         ball.restart()
-        ball.increase_speed()
         ball.bounce_x()
         right_score_board.clear()
         right_score_board.add_point()
